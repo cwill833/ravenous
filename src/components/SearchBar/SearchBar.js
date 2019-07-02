173e448efc85deadebd33db69c12f191df3c4bbe
Chris Williams <cwill833@gmail.com>
2019-07-01 22:09:37 -0500

    searchBar component is updating color when clicked and loging out what user input with search algo

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,20 +1,59 @@
 import React, {Component} from 'react'
 import './SearchBar.css'
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rating': 'rating',
-    'Most Reviewed': 'review_count',
-}
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        }
+        this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest Rating': 'rating',
+            'Most Reviewed': 'review_count',
+        }
+
+        this.handleTermChange = this.handleTermChange.bind(this)
+        this.handleLocationChange = this.handleLocationChange.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
+
+    }
+    handleTermChange(event){
+        this.setState({term: event.target.value})
+    }
+
+    handleLocationChange(event){
+        this.setState({location: event.target.value})
+    }
+
+    handleSortByChange(sortByOption){
+        this.setState({sortBy: sortByOption})
+    }
+
+    handleSearch(event){
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+        event.preventDefault()
+    }
+
+    getSortByClass(sortByOption){
+        if(this.state.sortBy === sortByOption){
+            return 'active'
+        } else {
+            return ''
+        }
+    }
+
     renderSortByOptions(){
-        return Object.keys(sortByOptions).map(sortByOption =>{
-            let sortByOptionValue = sortByOptions[sortByOption]
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+        return Object.keys(this.sortByOptions).map(sortByOption =>{
+            let sortByOptionValue = this.sortByOptions[sortByOption]
+            return <li onClick={this.handleSortByChange.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
         })
     }
 
+    
     render(){
         return(
             <div className="SearchBar">
@@ -24,11 +63,11 @@ class SearchBar extends Component {
                 </ul>
             </div>
             <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" />
-                <input placeholder="Where?" />
+                <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+                <input onChange={this.handleLocationChange} placeholder="Where?" />
             </div>
             <div className="SearchBar-submit">
-                <a>Let's Go</a>
+                <a onClick={this.handleSearch}>Let's Go</a>
             </div>
             </div>
         )
